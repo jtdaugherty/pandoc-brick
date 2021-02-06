e6c86c7111c4db0e5007e0308379eb287d13d62f
Jonathan Daugherty <cygnus@foobox.com>
2021-02-05 21:06:32 -0800

    Add demo program to test rendering of markdown

--- demos/Main.hs
+++ demos/Main.hs
@@ -1,7 +1,59 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Brick
 import Brick.Widgets.Pandoc
+import Control.Monad (void)
+import qualified Data.Text.IO as T
+import qualified Graphics.Vty as V
+import System.Exit (exitFailure)
+import System.Environment (getArgs, getProgName)
+
+import Commonmark (commonmark)
+import Commonmark.Pandoc (Cm(unCm))
+import Text.Pandoc.Builder (Blocks)
+
+showHelp :: IO ()
+showHelp = do
+    pn <- getProgName
+    putStrLn $ "Usage: " ++ pn ++ " <input.md>"
+
+theme :: AttrMap
+theme =
+    attrMap V.defAttr
+    [ (pandocEmphAttr,       fg V.white)
+    , (pandocUnderlineAttr,  V.defAttr `V.withStyle` V.underline)
+    , (pandocStrongAttr,     V.defAttr `V.withStyle` V.bold)
+    , (pandocStrikeoutAttr,  V.defAttr `V.withStyle` V.strikethrough)
+    , (pandocInlineCodeAttr, fg V.magenta)
+    ]
+
+draw :: Blocks -> [Widget ()]
+draw bs =
+    [viewport () Vertical $ cached () $ renderPandoc bs]
+
+app :: App Blocks e ()
+app =
+    App { appDraw = draw
+        , appChooseCursor = showFirstCursor
+        , appHandleEvent = resizeOrQuit
+        , appStartEvent = return
+        , appAttrMap = const theme
+        }
 
 main :: IO ()
-main = simpleMain (renderPandoc mempty :: Widget ())
+main = do
+    args <- getArgs
+
+    path <- case args of
+        [p] -> return p
+        _ -> showHelp >> exitFailure
+
+    contents <- T.readFile path
+
+    case commonmark path contents of
+        Left e -> do
+            print e
+            exitFailure
+        Right (cm::Cm () Blocks) ->
+            void $ defaultMain app (unCm cm)
